44e8573f061e959531738ca95e2a0680b0bdeedc
humanamburu <Uladzimir_Halushka@epam.com>
2018-08-12 16:11:31 +0300

    Add tryHard, submit and callbackotify funcs

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # kostil.js
 
-The best library with workarounds and hacks
+It's like lodash but for the next level of JS code.
+
+
+## LICENSE
+
+Licensed under [WTFPL](http://www.wtfpl.net/) license
+
--- index.js
+++ index.js
@@ -9,7 +9,24 @@ export default function fix (f) {
 }
 
 /**
+ * Takes a function following the common promise style,
+ * and returns function in error-first style
+ * i.e. taking a (err, value) => ... callback as the last argument.
+ *
+ * @param {function} f
+ * @returns {Function}
+ */
+export function callbackotify(f) {
+    return (callback, ...args) => {
+        f(...args)
+            .then(result => callback(null, result))
+            .catch(err => callback(err, null));
+    };
+}
+
+/**
  * Evaluates code in the safe way!
+ *
  * @param {String} code
  */
 export function safeEval (code) {
@@ -30,4 +47,27 @@ export function safeEval (code) {
  */
 export function makeUnderstandableBoolean (boolean) {
     return boolean ? 'yes' : 'no';
+}
+
+/**
+ * Submits your form if you don't know about type="submit"
+ * @param form
+ * @param button
+ */
+export function submitForm (form, button) {
+    return button.addEventListener('click', () => form.submit());
+}
+
+/**
+ * Tries to call THE FUNCTION HARDER!1
+ * @param f
+ */
+export function tryHard (f) {
+    while (true) {
+        try {
+            return f();
+        } catch (e) {
+            console.log('Try harder!');
+        }
+    }
 }
